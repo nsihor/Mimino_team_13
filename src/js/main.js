4b1ea36904f19d8de369308b4eab0212e3fc40be
nsihor <nsihor@gmail.com>
2023-02-28 22:45:38 +0200

    add js files

---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+$('.hotel-hero-slider').owlCarousel({
+    items: 1.6,
+    loop: true,
+    nav: true,
+    dots: false,
+    merge: true,
+    margin: 32,
+    responsive: {
+        0: {
+            margin: 20,
+            items: 1.38,
+        },
+        768: {
+            margin: 32,
+        },
+    }
+})
+​
+$('.rest-hero-slider').owlCarousel({
+    items: 1.6,
+    loop: true,
+    nav: true,
+    dots: false,
+    merge: true,
+    margin: 32,
+    responsive: {
+        0: {
+            margin: 20,
+            items: 1.38,
+        },
+        768: {
+            margin: 32,
+        },
+    }
+})
+​
+$('.room-hero-slider').owlCarousel({
+    items: 1.6,
+    loop: true,
+    nav: true,
+    dots: false,
+    merge: true,
+    margin: 32,
+    responsive: {
+        0: {
+            margin: 20,
+            items: 1.38,
+        },
+        768: {
+            margin: 32,
+        },
+    }
+})
--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import "./js/main";
 import "./js/slick";
 import "./js/mobile-menu";
 import "./js/modal";
+import "./js/owl.carousel";
+import "./js/owl.carousel.min.js";
